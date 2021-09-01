6415c688545bef944e9c427be2a665f4d46f59fa
Harald Ringvold <harald.ringvold@gmail.com>
2021-09-01 02:09:36 +0200

    Serve Elm app on index page page

--- lib/get5_api_web/endpoint.ex
+++ lib/get5_api_web/endpoint.ex
@@ -23,8 +23,8 @@ defmodule Get5ApiWeb.Endpoint do
   plug Plug.Static,
     at: "/",
     from: :get5_api,
-    gzip: false,
-    only: ~w(css fonts images js favicon.ico robots.txt)
+    gzip: true,
+    only: ~w(css fonts images js favicon.ico robots.txt main.js dist)
 
   # Code reloading can be explicitly enabled under the
   # :code_reloader configuration of your endpoint.
