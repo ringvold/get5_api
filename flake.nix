c216e08fc63c9cf9b370af3e44db3ec5ba5fdb41
Harald Ringvold <harald.ringvold@gmail.com>
2022-07-18 23:14:13 +0200

    Kinda working nix flake
    Does not include postgres yet so its recommended to use docker-compose
    for that still (`docker-compose up db`).

--- flake.nix
+++ flake.nix
@@ -1,11 +1,5 @@
 {
-  description = "A Phoenix project";
-
-  inputs.pre-commit-hooks = {
-    url = "github:cachix/pre-commit-hooks.nix";
-    inputs.nixpkgs.follows = "nixpkgs";
-    inputs.flake-utils.follows = "flake-utils";
-  };
+  description = "Get5API";
 
   outputs = {
     self,
