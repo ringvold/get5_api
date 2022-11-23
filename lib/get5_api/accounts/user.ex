df24958c617ea02f768ccbdd5f9821d2be0afea9
Harald Ringvold <harald.ringvold@gmail.com>
2022-11-23 22:22:57 +0100

    Add changes to user module from new auth generator

--- lib/get5_api/accounts/user.ex
+++ lib/get5_api/accounts/user.ex
@@ -1,8 +1,10 @@
 defmodule Get5Api.Accounts.User do
   use Ecto.Schema
   import Ecto.Changeset
+
   @primary_key {:id, :binary_id, autogenerate: true}
   @foreign_key_type :binary_id
+
   schema "users" do
     field :email, :string
     field :password, :string, virtual: true, redact: true
@@ -28,21 +30,26 @@ defmodule Get5Api.Accounts.User do
       password field is not desired (like when using this changeset for
       validations on a LiveView form), this option can be set to `false`.
       Defaults to `true`.
+
+    * `:validate_email` - Validates the uniqueness of the email, in case
+      you don't want to validate the uniqueness of the email (like when
+      using this changeset for validations on a LiveView form before
+      submitting the form), this option can be set to `false`.
+      Defaults to `true`.
   """
   def registration_changeset(user, attrs, opts \\ []) do
     user
     |> cast(attrs, [:email, :password])
-    |> validate_email()
+    |> validate_email(opts)
     |> validate_password(opts)
   end
 
-  defp validate_email(changeset) do
+  defp validate_email(changeset, opts) do
     changeset
     |> validate_required([:email])
     |> validate_format(:email, ~r/^[^\s]+@[^\s]+$/, message: "must have the @ sign and no spaces")
     |> validate_length(:email, max: 160)
-    |> unsafe_validate_unique(:email, Get5Api.Repo)
-    |> unique_constraint(:email)
+    |> maybe_validate_unique_email(opts)
   end
 
   defp validate_password(changeset, opts) do
@@ -70,15 +77,25 @@ defmodule Get5Api.Accounts.User do
     end
   end
 
+  defp maybe_validate_unique_email(changeset, opts) do
+    if Keyword.get(opts, :validate_email, true) do
+      changeset
+      |> unsafe_validate_unique(:email, Get5Api.Repo)
+      |> unique_constraint(:email)
+    else
+      changeset
+    end
+  end
+
   @doc """
   A user changeset for changing the email.
 
   It requires the email to change otherwise an error is added.
   """
-  def email_changeset(user, attrs) do
+  def email_changeset(user, attrs, opts \\ []) do
     user
     |> cast(attrs, [:email])
-    |> validate_email()
+    |> validate_email(opts)
     |> case do
       %{changes: %{email: _}} = changeset -> changeset
       %{} = changeset -> add_error(changeset, :email, "did not change")
