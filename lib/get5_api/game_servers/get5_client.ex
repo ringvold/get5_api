79fde32b20d8f2d414202db536e4c0d232947ffd
Harald Ringvold <harald.ringvold@gmail.com>
2022-12-10 02:53:16 +0100

    Add url to start match
    There should probably not be references to the web module in this part
    of the app but we'll fix that later.

--- lib/get5_api/game_servers/get5_client.ex
+++ lib/get5_api/game_servers/get5_client.ex
@@ -9,8 +9,8 @@ defmodule Get5Api.GameServers.Get5Client do
   end
 
   def start_match(conn, match) do
-    url = ""
-    command = "get5_loadmatch_url \"\" \"Authorization\" \"Bearer #{match.api_key}\""
+    url = Get5Api.Endpoint.url <> "/matches/#{match.id}/match_config"
+    command = "get5_loadmatch_url \"#{url}\" \"Authorization\" \"Bearer #{match.api_key}\""
     Rcon.exec(conn, command)
   end
 
