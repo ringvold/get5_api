77488374cb452c1a7072f280d4d433f914b7c794
Harald Ringvold <harald.ringvold@gmail.com>
2023-04-18 01:51:31 +0200

    Add IP override possibility through env var

--- config/runtime.exs
+++ config/runtime.exs
@@ -10,7 +10,17 @@ if System.get_env("GET5API_IP_FIX") == "true" do
   ip = "#{a}.#{b}.#{c}.#{d}"
   IO.puts "IP address: #{ip}"
   config :get5_api, Get5ApiWeb.Endpoint,
-  url: [host: ip]
+    url: [host: ip]
+end
+
+# Override IP used by phoenix. Usefull for testing get5 event/callbacks over
+# tailscale.
+if System.get_env("GET5API_IP") != "" do
+  IO.puts "Using fixed IP addess from GET5API_IP env"
+  ip = System.get_env("GET5API_IP")
+  IO.puts "IP address: #{ip}"
+  config :get5_api, Get5ApiWeb.Endpoint,
+    url: [host: ip]
 end
 
 
